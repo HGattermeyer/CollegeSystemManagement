b663280d93ca89d9016c41c85db63609d12ed5b5
HGattermeyer <henrique1989@gmail.com>
2021-11-22 01:38:54 -0300

    updated front-end

--- CollegeSystemSystem/Models/Course.cs
+++ CollegeSystemSystem/Models/Course.cs
@@ -1,9 +1,14 @@
-﻿namespace CollegeSystemSystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CollegeSystemSystem.Models
 {
     public class Course
     {
         public int Id { get; set; }
 
+        [Display(Name="Course")]
+        [Required(ErrorMessage="{0} is required.")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}.")]
         public string CourseName { get; set; }
         public IEnumerable<Student> Students { get; set; } = new List<Student>();
         public IEnumerable<Subject> Subjects { get; set; } = new List<Subject>();
--- CollegeSystemSystem/Models/Person.cs
+++ CollegeSystemSystem/Models/Person.cs
@@ -1,9 +1,16 @@
-﻿namespace CollegeSystemSystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CollegeSystemSystem.Models
 {
     public class Person
     {
+
         public int Id { get; set; }
+        [Display(Name = "Name")]
+        [Required(ErrorMessage = "{0} is required.")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}.")]
         public string Name { get; set; }
+        [Display(Name = "Birth Date")]
         public DateTime BirthDate { get; set; }
     }
 }
